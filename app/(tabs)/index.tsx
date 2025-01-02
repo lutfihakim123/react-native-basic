3dc47af945b9a551301d750f111d97a9241dafff
arka.lutfi.hakim <lutfi.rahman@arkamaya.co.id>
2025-01-02 11:28:24 +0700

    add basic typography, margin, padding and flexbox

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,31 +5,68 @@ import { styles } from '../../assets/styles/styles';
 
 // Functional Component
 export default function HomeScreen() {
+
+  // react native basic
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>React Native Dasar</Text>
+  //     <StatusBar style="auto" />
+  //   </View>
+  // );
+
+  // Typography
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Typography</Text>
+
+  //     <br />
+
+  //     <Text>Font Size</Text>
+
+  //     {/* font size */}
+  //     <Text style={{ fontSize: 10 }}>Lutfi Rahman Hakim</Text>
+  //     <Text style={{ fontSize: 20 }}>Lutfi Rahman Hakim</Text>
+  //     <Text style={{ fontSize: 30 }}>Lutfi Rahman Hakim</Text>
+
+  //     <br />
+  //     <Text>Font Weight</Text>
+  //     {/* font weight */}
+  //     <Text style={{ fontWeight: 'normal' }}>Lutfi Rahman Hakim</Text>
+  //     <Text style={{ fontWeight: 'bold' }}>Lutfi Rahman Hakim</Text>
+
+  //     <br />
+  //     <Text>Font Style</Text>
+  //     {/* font style */}
+  //     <Text style={{ fontStyle: 'normal' }}>Lutfi Rahman Hakim</Text>
+  //     <Text style={{ fontStyle: 'italic' }}>Lutfi Rahman Hakim</Text>
+
+  //   </View>
+  // );
+
+  // Margin And Padding
+  // return (
+  //   <View style={styles.container}>
+  //     <Text>Margin And Padding</Text>
+  //     <View style={styles.card}>
+  //       <View style={styles.content}>
+  //         <Text>Hello World</Text> 
+  //       </View>
+  //     </View>
+  //   </View>
+  // );
+
+  // FlexBox
   return (
     <View style={styles.container}>
-      <Text>React Native Dasar</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapperKotak}>
+          {/* kotak merah */}
+          <View style={styles.merah}></View>
+          {/* kotak biru */}
+          <View style={styles.biru}></View>
+          {/* kotak hijau */}
+          <View style={styles.hijau}></View>
+      </View>
     </View>
   );
+  
 }
-
-// Arrow Functional Component
-// const index = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Text>React Native Dasar</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   )
-// }
-
-// Class Component 
-// export default class index extends Component {
-//   render() {
-//     return (
-//       <View>
-//         <Text>index</Text>
-//       </View>
-//     )
-//   }
-// }
--- assets/styles/styles.js
+++ assets/styles/styles.js
@@ -4,7 +4,35 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
+    },
+    card: {
+        backgroundColor : 'red',
+        padding: 30,
+        margin: 30,
+    },
+    content: {
+        backgroundColor: 'yellow',
+    },
+    merah: {
+        backgroundColor : 'red',
+        width: 100,
+        height: 100,
+    },
+    biru: {
+        backgroundColor : 'blue',
+        width: 100,
+        height: 70,
+    },
+    hijau: {
+        backgroundColor : 'green',
+        width: 100,
+        height: 100,
+    },
+    wrapperKotak: {
+        // flexDirection: 'row',
+        backgroundColor: 'yellow',
         alignItems: 'center',
         justifyContent: 'center',
-    },
+        flex: 1,
+    }
 });
